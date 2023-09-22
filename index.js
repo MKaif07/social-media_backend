43a30bfbe9687f6911509af5936a460b4dab1509
Kaif <kaif2002mk@gmail.com>
2023-09-22 09:11:32 +0530

    deleted build

--- index.js
+++ index.js
@@ -36,8 +36,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // );
 
 app.use(cors());
+
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use(express.static(path.resolve(__dirname, "build")));
+// app.use(express.static(path.resolve(__dirname, "build")));
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
